703d74c59b66fdadbc42617d9c2f2224a3b0c33f
Rumbustious <rumbustious0@gmail.com>
2024-11-13 14:36:51 +0200

    Checks Java & JavaFX Versions

--- src/main/java/rumb/typetocode/HelloApplication.java
+++ src/main/java/rumb/typetocode/HelloApplication.java
@@ -2,6 +2,7 @@ package rumb.typetocode;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,6 +13,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+//        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/rumb/typetocode/HelloController.java
+++ src/main/java/rumb/typetocode/HelloController.java
@@ -3,12 +3,16 @@ package rumb.typetocode;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        var javaVersion = SystemInfo.javaVersion();
+        var javafxVersion = SystemInfo.javafxVersion();
+        welcomeText.setText("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
     }
 }
