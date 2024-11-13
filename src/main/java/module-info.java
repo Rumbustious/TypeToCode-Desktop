16e37ead6d0579925086e92992db0883d93683be
Rumbustious <rumbustious0@gmail.com>
2024-11-13 13:32:31 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module rumb.typetocode {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens rumb.typetocode to javafx.fxml;
+    exports rumb.typetocode;
+}
---
+++ src/main/java/rumb/typetocode/HelloController.java
@@ -0,0 +1,14 @@
+package rumb.typetocode;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
